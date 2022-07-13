401576d5eafd6b25de52597b12a5499bd20b1d9c
Sean Hammond <seanh@users.noreply.github.com>
2022-07-13 16:26:56 +0100

    Add tests

--- src/tox_faster/main.py
+++ src/tox_faster/main.py
@@ -7,7 +7,6 @@ hookimpl = pluggy.HookimplMarker("tox")
 
 @hookimpl
 def tox_runenvreport(venv, action):  # pylint:disable=unused-argument
-    # pragma: no cover
     if "CI" in environ:
         return None
 
--- tests/unit/tox_faster/main_test.py
+++ tests/unit/tox_faster/main_test.py
@@ -1,3 +1,21 @@
+from unittest.mock import sentinel
+
+import pytest
+
+from tox_faster import main
+
+
 class TestToxRunEnvReport:
-    def test_it(self):
-        assert True
+    @pytest.mark.parametrize(
+        "environ,expected",
+        [
+            # In dev there' no CI envvar and tox-faster disables the envreport.
+            ({}, []),
+            # On GitHub Actions CI is set to "true" and tox-faster doesn't disable the envreport.
+            ({"CI": "true"}, None),
+        ],
+    )
+    def test_it(self, mocker, environ, expected):
+        mocker.patch.dict(main.environ, environ)
+
+        assert main.tox_runenvreport(sentinel.venv, sentinel.action) == expected
