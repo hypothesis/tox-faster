e3da80413e3ac5107dde319abcef9ca467c0208c
Sean Hammond <seanh@users.noreply.github.com>
2022-07-13 17:03:51 +0100

    Remove unnecessary test class

--- tests/unit/tox_faster/main_test.py
+++ tests/unit/tox_faster/main_test.py
@@ -5,17 +5,16 @@ import pytest
 from tox_faster import main
 
 
-class TestToxRunEnvReport:
-    @pytest.mark.parametrize(
-        "environ,expected",
-        [
-            # In dev there's no CI envvar and tox-faster disables the envreport.
-            ({}, []),
-            # On GitHub Actions CI is set to "true" and tox-faster doesn't disable the envreport.
-            ({"CI": "true"}, None),
-        ],
-    )
-    def test_it(self, mocker, environ, expected):
-        mocker.patch.dict(main.environ, environ)
+@pytest.mark.parametrize(
+    "environ,expected",
+    [
+        # In dev there's no CI envvar and tox-faster disables the envreport.
+        ({}, []),
+        # On GitHub Actions CI is set to "true" and tox-faster doesn't disable the envreport.
+        ({"CI": "true"}, None),
+    ],
+)
+def test_tox_runenvreport(mocker, environ, expected):
+    mocker.patch.dict(main.environ, environ)
 
-        assert main.tox_runenvreport(sentinel.venv, sentinel.action) == expected
+    assert main.tox_runenvreport(sentinel.venv, sentinel.action) == expected
